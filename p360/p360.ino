6f283c84aa6ecb0c9a42ef3040d42629b477cd46
Giosuè Russo <josh@jBook.station>
2016-03-01 12:13:16 +0100

    homing wip

--- p360/p360.ino
+++ p360/p360.ino
@@ -7,9 +7,11 @@
 // The X Stepper pins
 #define LON_STEPPER_STEP_PIN 2
 #define LON_STEPPER_DIR_PIN 3
+#define LON_STEPPER_HOMING_PIN 4
 // The Y stepper pins
 #define LAT_STEPPER_STEP_PIN 6
 #define LAT_STEPPER_DIR_PIN 7
+#define LAT_STEPPER_HOMING_PIN 8
 
 #define SHUTTER_PIN 13
 #define START_BTN_PIN 9
@@ -26,20 +28,18 @@ int currentPtInx = 0;
 
 int incomingByte = 0;
 int shutterTime = 10;
-bool runing = true;
+
+bool runing = false;
+int homingStatus = 3; // 0=off - 1=homing lon - 2=homing lat
+
+int lonStepPerRevolution = LON_STEP_PER_REVOLUTION;
+int latStepPerRevolution = LAT_STEP_PER_REVOLUTION;
 
 void setup()
 {
   pinMode(SHUTTER_PIN, OUTPUT);
-
-  lonStepper.setMaxSpeed(LON_STEP_PER_REVOLUTION / 10); // piatto
-  lonStepper.setAcceleration(LON_STEP_PER_REVOLUTION / 20);
-  // lonStepper.moveTo(0);
-  
-  latStepper.setMaxSpeed(LAT_STEP_PER_REVOLUTION / 30); // macchina fotografica
-  latStepper.setAcceleration(LAT_STEP_PER_REVOLUTION / 40);
-  // latStepper.moveTo(0);
-
+  pinMode(LON_STEPPER_HOMING_PIN, INPUT);
+  pinMode(LAT_STEPPER_HOMING_PIN, INPUT);
 
   Serial.begin(9600);
   
@@ -52,68 +52,172 @@ void setup()
   //   Serial.println();
   // }
 
-  int lonSteps = radToStep( getLon(0), LON_STEP_PER_REVOLUTION );
-  int latSteps = radToStep( getLat(0), LAT_STEP_PER_REVOLUTION );
+  resetMotors();
 
-  lonStepper.setCurrentPosition(lonSteps);
-  latStepper.setCurrentPosition(latSteps);
+}
+
+void doHoming(){
+  if(homingStatus <= 0) return;
+
+  if(homingStatus == 1){
+    
+    if(lonStepper.distanceToGo() == 0){
+      int nextPosition = lonStepper.currentPosition() + 1000;
+      lonStepper.moveTo(nextPosition);
+    }
+
+    int lonHomingTriggered = !digitalRead(LON_STEPPER_HOMING_PIN);
+    if(lonHomingTriggered && lonStepper.currentPosition() > 300 ){ 
+      // lonStepper.moveTo(lonStepper.currentPosition());  
+      
+      Serial.print("first zero at ");
+      Serial.println(lonStepper.currentPosition());
+
+      lonStepper.setCurrentPosition(0);
+      lonStepper.moveTo(0);
+
+      homingStatus++;
+    }
+
+  }else if(homingStatus == 2){ 
+    if(lonStepper.distanceToGo() == 0){
+      int nextPosition = lonStepper.currentPosition() + 1000;
+      lonStepper.moveTo(nextPosition);
+    }
+    int lonHomingTriggered = !digitalRead(LON_STEPPER_HOMING_PIN);
+    if(lonHomingTriggered && lonStepper.currentPosition() > 300 ){
+
+      lonStepPerRevolution = lonStepper.currentPosition();
+
+      Serial.print("lonStepPerRevolution: ");
+      Serial.println(lonStepPerRevolution);
+
+      // lonStepper.setCurrentPosition(0);
+      lonStepper.moveTo(0);
+
+      homingStatus++; 
+    }
+  }else if(homingStatus == 3){
+    
+    if(latStepper.distanceToGo() == 0){
+      int nextPosition = latStepper.currentPosition() + 1000;
+      latStepper.moveTo(nextPosition);
+    }
+
+    int latHomingTriggered = !digitalRead(LAT_STEPPER_HOMING_PIN);
+    if(latHomingTriggered && latStepper.currentPosition() > 300 ){ 
+      // latStepper.moveTo(latStepper.currentPosition());  
+      
+      Serial.print("first zero at ");
+      Serial.println(latStepper.currentPosition());
+
+      latStepper.setCurrentPosition(0);
+      latStepper.moveTo(0);
+
+      homingStatus++;
+    }
+
+  }else if(homingStatus == 4){ 
+    if(latStepper.distanceToGo() == 0){
+      int nextPosition = latStepper.currentPosition() + 1000;
+      latStepper.moveTo(nextPosition);
+    }
+    int latHomingTriggered = !digitalRead(LAT_STEPPER_HOMING_PIN);
+    if(latHomingTriggered && latStepper.currentPosition() > 300 ){
+
+      latStepPerRevolution = latStepper.currentPosition();
+
+      Serial.print("latStepPerRevolution: ");
+      Serial.println(latStepPerRevolution);
+
+      // latStepper.setCurrentPosition(0);
+      latStepper.moveTo(0);
+
+      homingStatus++; 
+    }
+  }
+
+
+
+}
+
+void resetMotors(){
+  lonStepper.setMaxSpeed(lonStepPerRevolution / 10); // piatto
+  lonStepper.setAcceleration(lonStepPerRevolution / 20);
+  // lonStepper.moveTo(0);
+  
+  latStepper.setMaxSpeed(latStepPerRevolution / 30); // macchina fotografica
+  latStepper.setAcceleration(latStepPerRevolution / 40);
+  // latStepper.moveTo(0);
+
+  // int lonSteps = radToStep( getLon(0), lonStepPerRevolution );
+  // int latSteps = radToStep( getLat(0), latStepPerRevolution );
+  // lonStepper.setCurrentPosition(lonSteps);
+  // latStepper.setCurrentPosition(latSteps);
+}
+
+void do360Spiral(){
+  if(!runing) return;
+
+  if(lonStepper.distanceToGo() == 0 && latStepper.distanceToGo() == 0){
+
+    delay(shutterTime);
+
+    digitalWrite(SHUTTER_PIN, 1);
+    delay(300);
+    digitalWrite(SHUTTER_PIN, 0);
+    delay(30);
+
+    int lonSteps = radToStep( getLon(currentPtInx), lonStepPerRevolution );
+    int latSteps = radToStep( getLat(currentPtInx), latStepPerRevolution );
+    
+    // lonSteps = loopStep(lonSteps, lonStepPerRevolution );
+
+    lonStepper.moveTo(lonSteps);
+    latStepper.moveTo(latSteps);
+    
+    Serial.print(currentPtInx);
+    Serial.print("\t");
+    Serial.print( getLon(currentPtInx) );
+    Serial.print("\t");
+    Serial.print( getLat(currentPtInx) );
+    Serial.println();
+
+    currentPtInx++;  
+    currentPtInx = currentPtInx % nbrPoints;   
+    
+    if(currentPtInx == 0){
+      runing = false;
+    }
+  
+  }
+
 
 }
 
 void loop()
 {
-    
-    if(runing && lonStepper.distanceToGo() == 0 && latStepper.distanceToGo() == 0){
-      delay(shutterTime);
+  
+  doHoming();
 
-      digitalWrite(SHUTTER_PIN, 1);
-      delay(300);
-      digitalWrite(SHUTTER_PIN, 0);
-      delay(30);
+  // if (Serial.available())
+  // {
+  //   int f = Serial.parseInt();  
 
-      int lonSteps = radToStep( getLon(currentPtInx), LON_STEP_PER_REVOLUTION );
-      int latSteps = radToStep( getLat(currentPtInx), LAT_STEP_PER_REVOLUTION );
-      
-      // lonSteps = loopStep(lonSteps, LON_STEP_PER_REVOLUTION );
-
-      lonStepper.moveTo(lonSteps);
-      latStepper.moveTo(latSteps);
-      
-      Serial.print(currentPtInx);
-      Serial.print("\t");
-      Serial.print( getLon(currentPtInx) );
-      Serial.print("\t");
-      Serial.print( getLat(currentPtInx) );
-      Serial.println();
-
-      currentPtInx++;  
-      currentPtInx = currentPtInx % nbrPoints;   
-      
-      if(currentPtInx == 0){
-        runing = false;
-      }
-    }
-    
-
-
-    // if (Serial.available())
-    // {
-    //   int f = Serial.parseInt();  
-
-    //   lonStepper.moveTo(f);
-    //   Serial.println(f); 
-    // } 
+  //   lonStepper.moveTo(f);
+  //   Serial.println(f); 
+  // } 
 
 
 
-    // if (lonStepper.distanceToGo() == 0)
-    //   lonStepper.moveTo(-lonStepper.currentPosition());
+  // if (lonStepper.distanceToGo() == 0)
+  //   lonStepper.moveTo(-lonStepper.currentPosition());
 
-    // if (latStepper.distanceToGo() == 0)
-    //   latStepper.moveTo(-latStepper.currentPosition());
+  // if (latStepper.distanceToGo() == 0)
+  //   latStepper.moveTo(-latStepper.currentPosition());
 
-    lonStepper.run();
-    latStepper.run();
+  lonStepper.run();
+  latStepper.run();
 }
 
 float getLon(int ptinx){
